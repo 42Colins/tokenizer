25af0746ad6884dc1b3e6b75716e938eb3d6ecef
Colin Projean <colinprojean@Colins-MacBook-Pro.local>
2025-02-05 21:31:48 +0100

    Working on multisig, need to deploy and test

--- code/smartContract.sol
+++ code/smartContract.sol
@@ -1,42 +1,195 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Importing ERC20 library
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/BEP20/IBEP20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-// Creating an error function for not being the owner of the contract
 error ErrorOwnerOnlyFunction();
+error InvalidSignersCount();
+error InvalidSignature();
+error TransactionAlreadyExecuted();
+error InsufficientConfirmations();
+error InvalidTransaction();
 
-contract FourtyTwotoTheMoon is ERC20, ERC20Permit {
+contract FourtyTwoToTheMoonMultisig is ERC20, ERC20Permit {
     address public owner;
     uint256 private constant INITIAL_SUPPLY = 42_000_000 * 10**18;
-
-    modifier onlyOwner() {      // Modifier to check if the user is the creator of the contract
+    
+    // Multisig parameters
+    uint256 public required;
+    address[] public signers;
+    mapping(address => bool) public authSigners;
+    
+    // Transaction structure
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        mapping(address => bool) confirmations;
+        uint256 numConfirmations;
+    }
+    
+    Transaction[] public waitingTransactions;
+    
+    // Events
+    event TransactionSubmitted(uint256 indexed txIndex, address indexed from, address indexed to, uint256 value);
+    event TransactionConfirmed(uint256 indexed txIndex, address indexed signer);
+    event TransactionExecuted(uint256 indexed txIndex);
+    
+    modifier onlyOwner() {
         if (msg.sender != owner)
             revert ErrorOwnerOnlyFunction();
         _;
     }
-
-    constructor() ERC20("42toTheMoon", "42Moon") ERC20Permit("42toTheMoon") { // Constructor for our BEP20 token named 42toTheMoon
+    
+    modifier onlySigner() {
+        require(authSigners[msg.sender], "Not a signer");
+        _;
+    }
+    
+    modifier txExists(uint256 _txIndex) {
+        require(_txIndex < waitingTransactions.length, "Transaction does not exist");
+        _;
+    }
+    
+    modifier notExecuted(uint256 _txIndex) {
+        if(waitingTransactions[_txIndex].executed) revert TransactionAlreadyExecuted();
+        _;
+    }
+    
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!waitingTransactions[_txIndex].confirmations[msg.sender], "Already confirmed");
+        _;
+    }
+    
+    constructor(address[] memory _signers, uint256 _required) 
+        ERC20("42toTheMoon", "42Moon") 
+        ERC20Permit("42toTheMoon") 
+    {
+        if(_required == 0 || _required > _signers.length) revert InvalidSignersCount();
+        
         owner = msg.sender;
         _mint(msg.sender, INITIAL_SUPPLY);
+        
+        for (uint256 i = 0; i < _signers.length; i++) {
+            address signer = _signers[i];
+            require(signer != address(0), "Invalid signer");
+            require(!authSigners[signer], "Signer not unique");
+            
+            authSigners[signer] = true;
+            signers.push(signer);
+        }
+        
+        required = _required;
     }
-
-    function mint(address dest, uint256 amount) public onlyOwner { // Function calling the BEP20 function _mint to produce the tokens
+    
+    function submitTransaction(address _to, uint256 _value)
+        public
+        onlySigner
+        returns (uint256)
+    {
+        require(authSigners[msg.sender], "Not authorized");
+        uint256 txIndex = waitingTransactions.length;
+        
+        waitingTransactions.push();
+        Transaction storage transaction = waitingTransactions[txIndex];
+        transaction.to = _to;
+        transaction.value = _value;
+        transaction.executed = false;
+        transaction.numConfirmations = 0;
+        
+        emit TransactionSubmitted(txIndex, msg.sender, _to, _value);
+        
+        return txIndex;
+    }
+    
+    function confirmTransaction(uint256 _txIndex)
+        public
+        onlySigner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Transaction storage transaction = waitingTransactions[_txIndex];
+        transaction.confirmations[msg.sender] = true;
+        transaction.numConfirmations += 1;
+        
+        emit TransactionConfirmed(_txIndex, msg.sender);
+    }
+    
+    function executeTransaction(uint256 _txIndex)
+        public
+        onlySigner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        Transaction storage transaction = waitingTransactions[_txIndex];
+        
+        if(transaction.numConfirmations < required) revert InsufficientConfirmations();
+        
+        
+        // Execute the transaction
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        if(!success) revert InvalidTransaction();
+        
+        transaction.executed = true;
+        emit TransactionExecuted(_txIndex);
+    }
+    
+    function mint(address dest, uint256 amount) public onlyOwner {
         _mint(dest, amount);
     }
-
-    function burn(uint256 amount) public { // Function calling the BEP20 function _burn used to burn tokens (reducing the total supply therefore making the token more rare)
+    
+    function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
-
-    // Transfering the token directly from an address to another
-    function transfer(address recipient, uint256 amount) external override returns (bool) {
-        _transfer(recipient, amount);
-        emit Transfer(msg.sender, recipient, amount);
+    
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
         return true;
     }
+    
+    // Helper functions
+    function getTransaction(uint256 _txIndex)
+        public
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data,
+            bool executed,
+            uint256 numConfirmations
+        )
+    {
+        Transaction storage transaction = waitingTransactions[_txIndex];
+        
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
+    }
+    
+    function getSigners() public view returns (address[] memory) {
+        return signers;
+    }
+    
+    function getSignerCount() public view returns (uint256) {
+        return signers.length;
+    }
 
-    // Transfering the token 
+    function addSigner(address _newSigner) external onlyOwner
+    {
+        authSigners[_newSigner] = true;
+    }
+
+    function removeSigner(address _signer) external onlyOwner
+    {
+        authSigners[_signer] = false;
+    }
 }
